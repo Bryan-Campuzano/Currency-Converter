4a7159a850534105130d0183dd9251b4dafd9933
Bryan-Campuzano <bryam11122113@gmail.com>
2024-08-09 12:09:42 -0500

    Add first version of lambda code
    This initial version converts USD to Euros

---
+++ lambda_function.py
@@ -0,0 +1,27 @@
+import json
+
+def lambda_handler(event, context):
+    # Obtener los parámetros de la solicitud, amount = 0 si no encuetra este valor
+    body = json.loads(event['body'])
+    amount = float(body.get('amount'))
+    from_currency = body.get('from_currency')
+    to_currency = body.get('to_currency')
+
+    # Tasa de cambio fija para el ejemplo
+    exchange_rates = {
+        'USD': {'EUR': 0.93},
+    }
+
+    # Calcular la conversión, si se encuentran las divisas de conversion dentro del diccionario
+    converted_amount = amount * exchange_rates[from_currency][to_currency]
+    response = {
+    # generamos el vuerpo de la respuesta del endpoint
+        'converted_amount': converted_amount,
+        'from_currency': from_currency,
+        'to_currency': to_currency
+        }
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response)
+    }
