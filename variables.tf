7677ab004645015289c9b1a6d51d0032aee81e35
Bryan-Campuzano <bryam11122113@gmail.com>
2024-09-27 21:41:25 -0500

    add terraform variables
    modify terraform variables file adding the data for the region, lambda name, api name and lambda bucket for the aws setting

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "function_name" {
+  default = "currency-converter-lambda"
+}
+
+variable "api_name" {
+  default = "currency-converter-api"
+}
+
+variable "lambda_s3_bucket" {
+  default = "my-lambda-bucket"
+}
