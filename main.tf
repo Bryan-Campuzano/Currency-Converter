2a907862108fd9c2504da1f471bf0fd88e3fc12c
Bryan-Campuzano <bryam11122113@gmail.com>
2024-09-27 22:15:04 -0500

    terraform provider and iam role setup

--- main.tf
+++ main.tf
@@ -1,3 +1,27 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com",
+        },
+      },
+    ],
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
+  ]
+}
 
 resource "aws_lambda_function" "lambda_function" {
   filename         = "lambda_function.zip"
